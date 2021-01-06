a349ecb8b8ba8bf0ff3edca370db38e3f9f64053
Eric Yuzo Nakasone <eric.nakasone@hotmail.com>
2021-01-06 23:39:38 +0200

    Added legends and ajusted the map size

--- assets/js/validation_map.js
+++ assets/js/validation_map.js
@@ -40,7 +40,7 @@ var stamenTerrain = new ol.layer.Tile({
 //GHS rec layer
 var GHS_rec = new ol.layer.Image({
 	title:'GHS reclassified',
-	visible:true,
+	visible:false,
 	source: new ol.source.ImageWMS({
 		url:"http://localhost:8082/geoserver/wms",
 		params: {'LAYERS' : 'Lab_project_pop:ghs_rec_int'}
@@ -50,12 +50,21 @@ var GHS_rec = new ol.layer.Image({
 //World pop rec layer
 var worldpop_rec = new ol.layer.Image({
 	title:'Worlpop reclassified',
-	visible:true,
+	visible:false,
 	source: new ol.source.ImageWMS({
 		url:"http://localhost:8082/geoserver/wms",
 		params: {'LAYERS' : 'Lab_project_pop:worldpop_rec_int'}
 	})
 });
+
+//Legend for reclassified maps
+var addLegend_rec = function () {
+  var graphicUrl = 'http://localhost:8082/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=Lab_project_pop:ghs_rec_int&LEGEND_OPTIONS=fontSize:11.5;dx:10;mx:0.1;my:0.1;bgColor:0x91b9d1';
+  var img = document.getElementById('legend_rec');
+  img.src = graphicUrl;
+};
+
+addLegend_rec();
 
 //Map of differences
 var diff_3scenarios = new ol.layer.Image({
@@ -67,10 +76,19 @@ var diff_3scenarios = new ol.layer.Image({
 	})
 });
 
+//Legend for reclassified map of differences
+var addLegend_valid_diff = function () {
+  var graphicUrl = 'http://localhost:8082/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=Lab_project_pop:diff_3scenarios_int&LEGEND_OPTIONS=fontSize:11.5;dx:10;mx:0.1;my:0.1;bgColor:0x91b9d1';
+  var img = document.getElementById('legend_diff');
+  img.src = graphicUrl;
+};
+
+addLegend_valid_diff();
+
 //Classification points
 var samp_points = new ol.layer.Image({
 	title:'Sampling points',
-	visible:true,
+	visible:false,
 	source: new ol.source.ImageWMS({
 		url:"http://localhost:8082/geoserver/wms",
 		params: {'LAYERS' : 'Lab_project_pop:classified_validation_vector_final'}
@@ -90,9 +108,9 @@ var map = new ol.Map({
 			})
 		],
 	view: new ol.View({
-		center:ol.proj.fromLonLat([144,47]),
-		zoom:3.3,
-		minZoom:3
+		center:ol.proj.fromLonLat([143,47]),
+		zoom:3.7,
+		minZoom:3.4
 	}),
 	controls: ol.control.defaults().extend([
 		new ol.control.ScaleLine(),
